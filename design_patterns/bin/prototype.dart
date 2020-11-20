03cf7f7e9faceaf29a975c91604d56b12b09709f
xuzhongpeng <1452754335@qq.com>
2020-11-20 17:44:11 +0800

    note-sync syncing

--- design_patterns/bin/prototype.dart
+++ design_patterns/bin/prototype.dart
@@ -2,7 +2,10 @@ import './run.dart';
 
 abstract class ProtoType{
   String _id;
-  
+  ProtoType(this._id);
+  String get id=>_id;
+  void set id(String id)=>this._id=id;
+  ProtoType clone();
 }
 
 class RunPrototype implements Run {
