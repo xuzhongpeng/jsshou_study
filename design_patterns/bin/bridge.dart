efe378759af574e7644ae6bed89f429632f3c3f1
xuzhongpeng <1452754335@qq.com>
2020-11-23 19:57:29 +0800

    note-sync syncing

--- design_patterns/bin/bridge.dart
+++ design_patterns/bin/bridge.dart
@@ -1,31 +1,30 @@
-
-
 import 'run.dart';
+
 //咖啡售卖系统中，容量作为抽象化（Abstraction），口味作为实现化（Implementor）
 //创建口味接口
 abstract class ICoffeeAdditives {
-    void addSomething();
+  void addSomething();
 }
+
 // 创建抽象化
 abstract class Coffee {
   ICoffeeAdditives additives;
   Coffee(this.additives);
   void orderCoffee(int count);
 }
+
 //修正抽象化类，增加品控方法
-abstract class RefinedCoffee extends Coffee{
+abstract class RefinedCoffee extends Coffee {
   RefinedCoffee(ICoffeeAdditives additives) : super(additives);
-  checkQuality(int score){
-    print("添加太多");
+  checkQuality(int score) {
+    print("添加" + score.toString() + "%");
   }
 }
 
-class RunBridge implements Run{
+class RunBridge implements Run {
   @override
-  String name="桥接模式";
+  String name = "桥接模式";
 
   @override
-  main() {
-
-  }
+  main() {}
 }
