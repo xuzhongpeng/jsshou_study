ca92c88ef98474610856d80e6d4651e44d195c12
xuzhongpeng <1452754335@qq.com>
2020-11-23 16:15:04 +0800

    note-sync syncing

--- flutter_demo/lib/single_page/back.dart
+++ flutter_demo/lib/single_page/back.dart
@@ -14,28 +14,43 @@ class _BackPageState extends State<BackPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: Text("返回测试"),
-    ));
+      appBar: AppBar(
+        title: Text("返回测试"),
+      ),
+      body: Column(
+        children: <Widget>[
+          RaisedButton(
+            child: Text("page1"),
+            onPressed: () => Navigator.of(context)
+                .push(MaterialPageRoute(builder: (_) => _Page1())),
+          ),
+          RaisedButton(
+            child: Text("page2"),
+            onPressed: () => Navigator.of(context)
+                .push(MaterialPageRoute(builder: (_) => _Page2())),
+          )
+        ],
+      ),
+    );
   }
 }
 
-class _Page1 extends StatefulWidget {
-  @override
-  _Page1State createState() => _Page1State();
-}
-
-class _Page1State extends State<_Page1> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
+class _Page1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
       title: Text("Page1"),
+    ));
+  }
+}
+
+class _Page2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      title: Text("Page2"),
     ));
   }
 }
