4d120b98f1731cbd92cfdad2c8498b15d8fc80d3
xuzhongpeng <1452754335@qq.com>
2020-11-23 12:53:17 +0800

    note-sync syncing

--- design_patterns/bin/adapter.dart
+++ design_patterns/bin/adapter.dart
@@ -1,7 +1,28 @@
 
 
 import 'run.dart';
+//官方媒体播放接口
+abstract class MediaPlayer{
+  void play(String audioType,String fileName);
+}
+//自定义媒体播放接口
+abstract class AdvancedMediaPlayer {
+  void playVlc(String fileName);
+  void playMp4(String fileName);
+}
+//创建实现了 AdvancedMediaPlayer 接口的实体类。
+class VlcPlayer implements AdvancedMediaPlayer{
+  @override
+  void playMp4(String fileName) {
+    // TODO: implement playMp4
+  }
 
+  @override
+  void playVlc(String fileName) {
+    // TODO: implement playVlc
+  }
+  
+}
 class RunAdapter implements Run {
   @override
   main() {
