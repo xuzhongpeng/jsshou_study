cf5660d082174ac132d55862aec95ae08747e9b3
xuzhongpeng <1452754335@qq.com>
2020-11-23 18:01:36 +0800

    note-sync syncing

--- flutter_demo/lib/componets/utils.dart
+++ flutter_demo/lib/componets/utils.dart
@@ -4,6 +4,8 @@ import './jnStatelessWidget.dart';
 
 class Utils {
   static push(BuildContext context, JnStatelessWidget widget) {
-    Navigator.of(context).push(MaterialPageRoute(builder: (_) => widget));
+    Navigator.of(context).push(MaterialPageRoute(
+        builder: (_) => widget,
+        settings: RouteSettings(name: widget.pageName)));
   }
 }
