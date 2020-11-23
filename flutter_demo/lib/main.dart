302509c3db9dd4ca65f8751ebebf3c70e7648c89
xuzhongpeng <1452754335@qq.com>
2020-11-23 17:00:14 +0800

    note-sync syncing

--- flutter_demo/lib/main.dart
+++ flutter_demo/lib/main.dart
@@ -136,18 +136,18 @@ class OwnNavigatorObserver extends NavigatorObserver {
   @override
   void didPop(Route route, Route previousRoute) {
     super.didPop(route, previousRoute);
-    print("wow， navigator has popped" + route.settings.name);
+    print("wow， navigator has popped" + route.settings.name.toString());
   }
 
   @override
   void didPush(Route route, Route previousRoute) {
     super.didPush(route, previousRoute);
-    print("wow, navigator has pushed" + route.settings.name);
+    print("wow, navigator has pushed" + route.settings.name.toString());
   }
 
   @override
   void didRemove(Route route, Route previousRoute) {
     super.didRemove(route, previousRoute);
-    print("wow, navigator has remove" + route.settings.name);
+    print("wow, navigator has remove" + route.settings.name.toString());
   }
 }
