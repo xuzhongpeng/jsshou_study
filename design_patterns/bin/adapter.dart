0dd0ad302f3e5b7d8080da18d3ca33699146602c
xuzhongpeng <1452754335@qq.com>
2020-11-23 12:59:06 +0800

    note-sync syncing

--- design_patterns/bin/adapter.dart
+++ design_patterns/bin/adapter.dart
@@ -76,7 +76,14 @@ class AudioPlayer implements MediaPlayer {
 
 class RunAdapter implements Run {
   @override
-  main() {}
+  main() {
+    AudioPlayer audioPlayer = new AudioPlayer();
+    audioPlayer.play("mp3", "beyond the horizon.mp3");
+    audioPlayer.play("mp4", "alone.mp4");
+    audioPlayer.play("vlc", "far far away.vlc");
+    audioPlayer.play("avi", "mind me.avi");
+  }
+
   @override
   String name = "适配器模式";
 }
