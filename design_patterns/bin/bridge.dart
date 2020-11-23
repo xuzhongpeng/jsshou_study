24d0e349f468a64398b94740a9206393cebdc4fb
xuzhongpeng <1452754335@qq.com>
2020-11-23 19:53:08 +0800

    note-sync syncing

--- design_patterns/bin/bridge.dart
+++ design_patterns/bin/bridge.dart
@@ -1,9 +1,11 @@
 
 
 import 'run.dart';
-//奶茶系统中，容量作为抽象化（Abstraction），口味作为实现化（Implementor）
+//咖啡售卖系统中，容量作为抽象化（Abstraction），口味作为实现化（Implementor）
 // 创建抽象化
-
+abstract class Coffee {
+  ICoffee
+}
 
 class RunBridge implements Run{
   @override
