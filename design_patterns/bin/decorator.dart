c0fe528ce3a7e8afa4f76dafc8b63d20df3109ac
xuzhongpeng <xuzhongpeng@foxmail.com>
2020-11-24 23:21:05 +0800

    note-sync syncing

--- design_patterns/bin/decorator.dart
+++ design_patterns/bin/decorator.dart
@@ -1,3 +1,20 @@
+import 'run.dart';
+//创建接口
+abstract class Shape {
+  void draw();
+}
+//创建接口实体类
+class Rectangle implements Shape {
+  @override
+  void draw() {
+    // TODO: implement draw
+  }
 
+}
 
-
+class RunDecorator implements Run {
+  @override
+  void main() {}
+  @override
+  String name = "";
+}
