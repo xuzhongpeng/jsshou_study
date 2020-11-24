038d048dedf14cbe2d24c6c01e229b7461e58689
xuzhongpeng <xuzhongpeng@foxmail.com>
2020-11-24 23:49:31 +0800

    note-sync syncing

--- design_patterns/bin/facade.dart
+++ design_patterns/bin/facade.dart
@@ -1,59 +1,66 @@
-
-
 import 'run.dart';
 // 创建一个接口。
 
 abstract class Shape {
-   void draw();
+  void draw();
 }
+
 // 创建实现接口的实体类。
 class Rectangle implements Shape {
- 
-   @override
-    void draw() {
-      print("Rectangle::draw()");
-   }
+  @override
+  void draw() {
+    print("Rectangle::draw()");
+  }
 }
- class Square implements Shape {
- 
-   @override
-    void draw() {
-      print("Square::draw()");
-   }
+
+class Square implements Shape {
+  @override
+  void draw() {
+    print("Square::draw()");
+  }
 }
- class Circle implements Shape {
- 
-   @override
-    void draw() {
-      print("Circle::draw()");
-   }
+
+class Circle implements Shape {
+  @override
+  void draw() {
+    print("Circle::draw()");
+  }
 }
+
 // 创建一个外观类。
- class ShapeMaker {
-    Shape circle;
-    Shape rectangle;
-    Shape square;
- 
-    ShapeMaker() {
-      circle = new Circle();
-      rectangle = new Rectangle();
-      square = new Square();
-   }
- 
-    void drawCircle(){
-      circle.draw();
-   }
-    void drawRectangle(){
-      rectangle.draw();
-   }
-    void drawSquare(){
-      square.draw();
-   }
+class ShapeMaker {
+  Shape circle;
+  Shape rectangle;
+  Shape square;
+
+  ShapeMaker() {
+    circle = new Circle();
+    rectangle = new Rectangle();
+    square = new Square();
+  }
+
+  void drawCircle() {
+    circle.draw();
+  }
+
+  void drawRectangle() {
+    rectangle.draw();
+  }
+
+  void drawSquare() {
+    square.draw();
+  }
 }
+
 class RunFacade implements Run {
   @override
   void main() {
+    ShapeMaker shapeMaker = new ShapeMaker();
+    shapeMaker.drawCircle();
+    shapeMaker.drawRectangle();
+    shapeMaker.drawSquare();
   }
+
   @override
-  String name="外观模式";
+  String name = "外观模式";
 }
