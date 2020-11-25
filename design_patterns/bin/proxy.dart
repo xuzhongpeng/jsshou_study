b614936c15e5b2b429fff40225c338731119e76f
xuzhongpeng <xuzhongpeng@foxmail.com>
2020-11-25 22:52:53 +0800

    note-sync syncing

--- design_patterns/bin/proxy.dart
+++ design_patterns/bin/proxy.dart
@@ -1,11 +1,39 @@
-
-
 import 'run.dart';
+
+abstract class Subject {
+  void request();
+}
+
+class RealSubject implements Subject {
+  @override
+  void request() {
+    print("RealSubject:Handling request");
+  }
+}
+
+class SubjectProxy implements Subject {
+  RealSubject _realSubject;
+  @override
+  void request() {
+    if (this._checkAccess()) {
+      this._realSubject.request();
+      this._logAccess();
+    }
+  }
+
+  bool _checkAccess() {
+    print("Proxy: Checking access prior to firing a real request.");
+    return true;
+  }
+
+  void _logAccess() {
+    print("Proxy: Logging the time of request.");
+  }
+}
 
 class RunProxy implements Run {
   @override
-  void main() {
-  }
+  void main() {}
   @override
-  String name="代理模式";
+  String name = "代理模式";
 }
