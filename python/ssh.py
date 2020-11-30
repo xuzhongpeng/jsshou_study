1c48b87887aac80a62a6c45364e90bd79890292d
xuzhongpeng <1452754335@qq.com>
2020-11-30 20:13:16 +0800

    note-sync syncing

---
+++ python/ssh.py
@@ -0,0 +1,20 @@
+import paramiko
+
+#功能：连接到linux的ssh服务器并运行一条命令
+#认证方式使用帐号密码
+def ssh_command(ip, user, passwd, command):
+    #创建客户端对象
+    client = paramiko.SSHClient()
+    #保存目标服务器的SSH密钥
+    client.set_missing_host_key_policy('/home/xy/.ssh/known_hosts')
+    # 使用帐号密码方式认证
+    client.connect(ip, username=user, password=passwd)
+    ssh_session = client.get_transport().open_session()
+    if ssh_session.active:
+        #执行命令
+        ssh_session.exec_command(command)
+        print(ssh_session.recv(1024))
+    return
+
+
+ssh_command('192.168.1.99', 'kali', 'lovepython', 'id')
