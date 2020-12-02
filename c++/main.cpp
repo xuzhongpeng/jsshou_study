e55375351476047f623e76492ab113037db884e9
xuzhongpeng <xuzhongpeng@foxmail.com>
2020-12-03 00:06:27 +0800

    note-sync syncing

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,7 +1,7 @@
-#include<iostream> //标准输入输出流库
-using namespace std;//c++命名空间
-int main()  //c++主函数,是程序执行的起点
-{
-    cout << "Hello world!" << endl; 
-    // return 0; //return 通常用于函数中,作为一个函数返回值!
+#include<stdio.h>
+
+int main() {
+    printf("hello world!");
+    getchar(); 
+    return 0; 
 }
