5d25c743fcceb79cd4246a19bdf6778b6e179b3f
xuzhongpeng <xuzhongpeng@foxmail.com>
2020-12-03 00:00:50 +0800

    note-sync syncing

--- c++/main.cpp
+++ c++/main.cpp
@@ -3,5 +3,5 @@ using namespace std;//c++命名空间
 int main()  //c++主函数,是程序执行的起点
 {
     cout << "Hello world!" << endl; 
-    return 0; //return 通常用于函数中,作为一个函数返回值!
+    // return 0; //return 通常用于函数中,作为一个函数返回值!
 }
