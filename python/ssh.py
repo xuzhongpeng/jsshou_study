05a7b1b27f4faa5e6ee270d4fc0e291ab1a17625
xuzhongpeng <1452754335@qq.com>
2020-12-02 10:17:06 +0800

    note-sync syncing

--- python/ssh.py
+++ python/ssh.py
@@ -1,4 +1,4 @@
-from paramiko  import paramiko,RejectPolicy
+import paramiko
 
 #功能：连接到linux的ssh服务器并运行一条命令
 #认证方式使用帐号密码
@@ -6,7 +6,7 @@ def ssh_command(ip, user, passwd, command):
     #创建客户端对象
     client = paramiko.SSHClient()
     #保存目标服务器的SSH密钥
-    client.set_missing_host_key_policy(RejectPolicy())
+    # client.set_missing_host_key_policy(RejectPolicy())
     # 使用帐号密码方式认证
     client.connect(ip, username=user, password=passwd)
     ssh_session = client.get_transport().open_session()
