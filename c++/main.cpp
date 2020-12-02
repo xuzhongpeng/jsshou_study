5dd1f3205d0303f77e43a5cd9bb964a3650b50c0
xuzhongpeng <xuzhongpeng@foxmail.com>
2020-12-03 00:12:34 +0800

    note-sync syncing

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,4 +1,5 @@
 #include<iostream> //标准输入输出流库
+#include<stdlib.h> //
 using namespace std;//c++命名空间
 int main()  //c++主函数,是程序执行的起点
 {
@@ -6,4 +7,5 @@ int main()  //c++主函数,是程序执行的起点
    cout << "请输入您的名称： ";
    cin >> name;
    cout << "您的名称是： " << name << endl;
+   system("pause");
 }
