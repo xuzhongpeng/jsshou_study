0bf13b0abb4c4ff6b517fa834e495fbb0bd6ab1d
xuzhongpeng <1452754335@qq.com>
2020-12-02 10:25:47 +0800

    note-sync syncing

--- python/ssh.py
+++ python/ssh.py
@@ -1,14 +1,17 @@
 import paramiko
+from paramiko import hostkeys
 
 #功能：连接到linux的ssh服务器并运行一条命令
 #认证方式使用帐号密码
 def ssh_command(ip, user, passwd, command):
     #创建客户端对象
     client = paramiko.SSHClient()
+    hostkeys = paramiko.HostKeys()
+    hostkeys.load('/Users/admin/.ssh/known_hosts')
     #保存目标服务器的SSH密钥
     # client.set_missing_host_key_policy(RejectPolicy())
     # 使用帐号密码方式认证
-    client.connect(ip, username=user, password=passwd)
+    client.connect(ip, username=user, password=passwd, hostkeys=hostkeys)
     ssh_session = client.get_transport().open_session()
     if ssh_session.active:
         #执行命令
