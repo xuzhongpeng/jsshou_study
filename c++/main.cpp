231055acd092b1dd2fbb3af15dc764d6b21610e3
xuzhongpeng <1452754335@qq.com>
2020-12-03 19:57:46 +0800

    note-sync syncing

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,19 +1,19 @@
-// #include<iostream> //标准输入输出流库
-// #include<stdlib.h> //
-// using namespace std;//c++命名空间
-// int main()  //c++主函数,是程序执行的起点
-// {
-//    char name[50];
-//    cout << "请输入您的名称： ";
-//    cin >> name;
-//    cout << "您的名称是： " << name << endl;
-//    system("pause");
-// }
-#include<stdio.h>
-
-int main() {
-    printf("hello world!");
-    int a= 123;
-    getchar(); 
-    return 0; 
+#include<iostream> //标准输入输出流库
+#include<stdlib.h> //
+using namespace std;//c++命名空间
+int main()  //c++主函数,是程序执行的起点
+{
+   char name[50];
+   cout << "请输入您的名称： ";
+   cin >> name;
+   cout << "您的名称是： " << name << endl;
+   system("pause");
 }
+// #include<stdio.h>
+
+// int main() {
+//     printf("hello world!");
+//     int a= 123;
+//     getchar(); 
+//     return 0; 
+// }
