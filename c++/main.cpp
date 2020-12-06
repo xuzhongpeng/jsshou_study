5e11d4598bfdf2b538ddf4705aeefe02c29e8e11
xuzhongpeng <xuzhongpeng@foxmail.com>
2020-12-06 18:04:46 +0800

    note-sync syncing

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,58 +1,11 @@
-#include <iostream>
-#include <cstdlib>
-#include <pthread.h>
-#include <unistd.h>
- 
-using namespace std;
- 
-#define NUM_THREADS     5
- 
-void *wait(void *t)
+#include<iostream> //æ ‡å‡†è¾“å…¥è¾“å‡ºæµåº“
+#include<stdlib.h> //
+using namespace std;//c++å‘½åç©ºé—´
+int main()  //c++ä¸»å‡½æ•°,æ˜¯ç¨‹åºæ‰§è¡Œçš„èµ·ç‚¹
 {
-   int i;
-   long tid;
- 
-   tid = (long)t;
- 
-   sleep(1);
-   cout << "Sleeping in thread " << endl;
-   cout << "Thread with id : " << tid << "  ...exiting " << endl;
-   pthread_exit(NULL);
-}
- 
-int main ()
-{
-   int rc;
-   int i;
-   pthread_t threads[NUM_THREADS];
-   pthread_attr_t attr;
-   void *status;
- 
-   // ³õÊ¼»¯²¢ÉèÖÃÏß³ÌÎª¿ÉÁ¬½ÓµÄ£¨joinable£©
-   pthread_attr_init(&attr);
-   pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
- 
-   for( i=0; i < NUM_THREADS; i++ ){
-      cout << "main() : creating thread, " << i << endl;
-      rc = pthread_create(&threads[i], NULL, wait, (void *)&i );
-      if (rc){
-         cout << "Error:unable to create thread," << rc << endl;
-         exit(-1);
-      }
-   }
- 
-   // É¾³ıÊôĞÔ£¬²¢µÈ´ıÆäËûÏß³Ì
-   pthread_attr_destroy(&attr);
-   for( i=0; i < NUM_THREADS; i++ ){
-      rc = pthread_join(threads[i], &status);
-      if (rc){
-         cout << "Error:unable to join," << rc << endl;
-         exit(-1);
-      }
-      cout << "Main: completed thread id :" << i ;
-      cout << "  exiting with status :" << status << endl;
-   }
- 
-   cout << "Main: program exiting." << endl;
-   pthread_exit(NULL);
+   char name[50];
+   cout << "è¯·è¾“å…¥æ‚¨çš„åç§°ï¼š ";
+   cin >> name;
+   cout << "æ‚¨çš„åç§°æ˜¯ï¼š " << name << endl;
+   system("pause");
 }
