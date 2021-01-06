bc858ef4ee089ce2426ff55a0c17690f095d67b5
xuzhongpeng <1452754335@qq.com>
2021-01-06 15:32:09 +0800

    note-sync syncing

--- python/timer.py
+++ python/timer.py
@@ -4,3 +4,10 @@ from subprocess import call
 cmd = 'display notification \"' + \
     "Notificaton memo" + '\" with title \"Titile\"'
 call(["osascript", "-e", cmd])
+def dojob():
+    # 创建调度器：BlockingScheduler
+    scheduler = BlockingScheduler()
+    forget()
+    scheduler.add_job(
+        forget, CronTrigger.from_crontab('0,30 20-22 * * 0-6'))
+    scheduler.start()
