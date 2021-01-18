817385196b0b216bf8a66912fea14d34fcec6b90
xuzhongpeng <1452754335@qq.com>
2021-01-18 21:04:45 +0800

    note-sync syncing

--- flutter_demo/bin/main.dart
+++ flutter_demo/bin/main.dart
@@ -32,11 +32,11 @@ void main() {
   // testReflection();
   String str =
       "https://api2.mc.app/index.php/system/qr_code?text=%7B%22type%22%3A%22s_bind%22%2C%22data%22%3A%7B%22key%22%3A%2295487612%22%2C%22company_name%22%3A%22J%26W+FASHION+DI+YAN+XINING%22%7D%7D&form=3";
-  str = str.replaceAll("%26", "replaceStr");
+  str = str.replaceAll("%26", "\${&}");
   Uri uri = Uri.tryParse(Uri.decodeQueryComponent(str));
 
   String key = uri?.queryParameters['text'];
-  key=key.replaceAll("replaceStr", "&");
+  key=key.replaceAll("\${&}", "&");
   print(key);
   // getAstTest();
 
