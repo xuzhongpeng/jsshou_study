61bb952d3be5f7ac190d206d9b95f17d9733f113
xuzhongpeng <1452754335@qq.com>
2021-01-18 09:43:58 +0800

    note-sync syncing

--- python/timer.py
+++ python/timer.py
@@ -5,10 +5,10 @@ def sendMessage(title:str,content:str):
     cmd = 'display notification \"' + \
     "Notificaton memo" + '\" with title \"Titile\"'
     call(["osascript", "-e", cmd])
-def dojob():
-    # 创建调度器：BlockingScheduler
-    scheduler = BlockingScheduler()
-    scheduler.add_job(
-        sendMessage, CronTrigger.from_crontab('0,30 20-22 * * 0-6'))
-    scheduler.start()
+# def dojob():
+#     # 创建调度器：BlockingScheduler
+#     scheduler = BlockingScheduler()
+#     scheduler.add_job(
+#         sendMessage, CronTrigger.from_crontab('0,30 20-22 * * 0-6'))
+#     scheduler.start()
 sendMessage()
