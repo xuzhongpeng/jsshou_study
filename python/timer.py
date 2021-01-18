9b01c353371bbf37fe739ff57508aa4044c8cf1c
xuzhongpeng <1452754335@qq.com>
2021-01-18 09:44:52 +0800

    note-sync syncing

--- python/timer.py
+++ python/timer.py
@@ -1,14 +1,18 @@
 from subprocess import call
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
-def sendMessage(title:str,content:str):
+
+
+def sendMessage(title: str, content: str):
     cmd = 'display notification \"' + \
-    "Notificaton memo" + '\" with title \"Titile\"'
+        content + '\" with title \"'+title+'\"'
     call(["osascript", "-e", cmd])
+
+
 # def dojob():
 #     # 创建调度器：BlockingScheduler
 #     scheduler = BlockingScheduler()
 #     scheduler.add_job(
 #         sendMessage, CronTrigger.from_crontab('0,30 20-22 * * 0-6'))
 #     scheduler.start()
-sendMessage('1','3')
+sendMessage('1', '3')
