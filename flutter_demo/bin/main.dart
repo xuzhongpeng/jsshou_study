ca5184d9eef3169cd5266520cd62f248c49184ba
xuzhongpeng <1452754335@qq.com>
2021-01-19 09:31:28 +0800

    note-sync syncing

--- flutter_demo/bin/main.dart
+++ flutter_demo/bin/main.dart
@@ -6,7 +6,7 @@ import 'dart_ast/getAst.dart';
 import 'fileManager.dart';
 import 'getFlutter.dart';
 import 'reflection/demo.dart';
-// import 'main.reflectable.dart';
+import 'main.reflectable.dart';
 
 String chart(List<int> items) {
   return jsonEncode({
@@ -28,16 +28,9 @@ void main() {
   // print(_createFileName(16));
   // print(DownFiles().getVersion('PRINT'));
 
-  // initializeReflectable();
-  // testReflection();
-  String str =
-      "https://api2.mc.app/index.php/system/qr_code?text=%7B%22type%22%3A%22s_bind%22%2C%22data%22%3A%7B%22key%22%3A%2295487612%22%2C%22company_name%22%3A%22J%26W+%3FFASHION+DI+YAN+XINING%22%7D%7D&form=3";
-  str = str.replaceAll("%26", "\${@}");
-  Uri uri = Uri.tryParse(Uri.decodeQueryComponent(str));
-
-  String key = uri?.queryParameters['text'];
-  key=key.replaceAll("\${@}", "&");
-  print(key);
+  initializeReflectable();
+  testReflection();
+  
   // getAstTest();
 
   // FileManager().getAllFilesPath();
