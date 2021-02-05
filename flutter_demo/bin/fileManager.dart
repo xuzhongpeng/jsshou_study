2bbf7d7b5a19f68f092cd28c2d0765f073f609fc
xuzhongpeng <1452754335@qq.com>
2021-02-05 12:39:55 +0800

    note-sync syncing

--- flutter_demo/bin/fileManager.dart
+++ flutter_demo/bin/fileManager.dart
@@ -85,11 +85,16 @@ class MySqlManager {
   }
 
   static Future<bool> insert(List<ClassMethod> method) async {
-    var result = await _conn.queryMulti(
+    var result ;
+    try{
+      result= await _conn.queryMulti(
         'insert into data (platform, type, class,method,comment,input,output,method_line_count,method_char_count,desc) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
         method.map((v) => v.toList()).toList()).catchError((MySqlException e){
           print(e.sqlState);
         });
+    }catch(e){
+      print(e);
+    }
     return result.length > 0;
   }
 
