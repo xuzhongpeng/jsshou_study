9a38cc7780ea789fb056ecccb480716b9c9c12ca
xuzhongpeng <1452754335@qq.com>
2021-02-05 11:52:44 +0800

    note-sync syncing

--- flutter_demo/bin/fileManager.dart
+++ flutter_demo/bin/fileManager.dart
@@ -81,7 +81,7 @@ class MySqlManager {
   static Future<bool> insert(ClassMethod method) async {
     var result = await conn.query(
         'insert into data (platform, type, class,method,comment,input,output,method_line_count,method_char_count,desc) values (?, ?, ?,?,?,?,?,?,?,?)',
-        [method.platform,'',method.className,method.annotation,method.params,method.returnParams,method.]);
+        [method.platform,'',method.className,method.annotation,method.params,method.returnParams,method.lineNumber,method.]);
     return result.length > 0;
   }
 }
